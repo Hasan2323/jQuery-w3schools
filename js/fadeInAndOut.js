4ae5fcf2a095408a95505c97a66f9ddf0ec6e3cb
Saimon <saimon.ctg@gmail.com>
2020-08-01 22:43:22 +0600

    fade in,out,toggle,to effects added

--- js/fadeInAndOut.js
+++ js/fadeInAndOut.js
@@ -1,5 +1,24 @@
 $(document).ready(function (){
+
+    //$(selector).fadeIn(speed,callback);
     $("#fadeInBtn").click(function (){
         $("#fadeInDiv").fadeIn(2000);
+    });
+
+    //$(selector).fadeOut(speed,callback);
+    $("#fadeOutBtn").click(function (){
+        $("#fadeOutDiv").fadeOut(2000);
+    });
+
+    //TODO: $(selector).fadeToggle(speed,callback);
+    $("#fadeInOutBtn").click(function (){
+        $("#fadeInOutDiv").fadeToggle(1500);
+    });
+
+    //TODO: $(selector).fadeTo(speed,opacity,callback);
+    $("#fadeToBtn").click(function (){
+        $("#fadeToDiv1").fadeTo("slow", 0.15);
+        $("#fadeToDiv2").fadeTo("slow", 0.5);
+        $("#fadeToDiv3").fadeTo("slow", 0.8);
     });
 });
